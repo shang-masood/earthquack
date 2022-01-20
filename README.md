4bcee212d068aeb27bea6f6d5eed503335d3384e
shang-masood <shangg.masood@gmail.com>
2022-01-20 14:10:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-u4fpyi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-u4fpyi)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+import Header from './components/header/Header';
+import Home from './components/pages/Home';
+import Route from './Route'
+export default function App() {
+  return (
+    <div>
+      <Header />
+    
+      <Route />
+    </div>
+  );
+}
